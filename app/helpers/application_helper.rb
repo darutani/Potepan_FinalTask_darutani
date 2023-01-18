7fd24b1eb1d66682ceadde2a2fdc18c45c2c6d4d
natsuki <su811sh1@gmail.com>
2023-01-18 15:01:35 +0900

    clean: RSpecでの位置引数を使用しない旨の指摘より、引数にデフォルトの値を持たせない形式に記載を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # ページ毎のtitle表示
-  def page_title(page_title="")
+  def page_title(page_title)
     base_title = "BIGBAG Store"
 
     if page_title.empty?
@@ -9,5 +9,4 @@ module ApplicationHelper
       page_title + " - " + base_title
     end
   end
-  
 end
