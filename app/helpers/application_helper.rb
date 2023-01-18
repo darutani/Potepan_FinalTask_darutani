40e36bbc3f78dea1bccbf1f569f53fa725140809
natsuki <su811sh1@gmail.com>
2023-01-18 09:12:23 +0900

    add: titleの表示方法をページ毎に異なるものへ変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # ページ毎のtitle表示
+  def page_title(page_title="")
+    base_title = "BIGBAG Store"
+
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
+  
 end
