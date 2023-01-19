f803cc768d9f8329585eeb1e387cca53a2f7cf52
natsuki <su811sh1@gmail.com>
2023-01-19 17:23:57 +0900

    update: 正しい商品情報が取得できているかのテストケースを追加

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe "Potepna::Products", type: :request do
   describe "GET /show" do
     let(:product) { create(:product) }
 
-    it "商品の詳細画面を表示すること" do
+    before do
       get potepan_product_path(product.id)
+    end
+
+    it "商品の詳細画面を表示すること" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "正しい商品情報が取得できていること" do
+      expect(response.body).to include product.name
+      expect(response.body).to include product.price.to_s
     end
   end
 end
