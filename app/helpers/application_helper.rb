fc841a1b44c4a35712388303611c0d6adaeb103b
natsuki <su811sh1@gmail.com>
2023-01-20 18:58:50 +0900

    clean: 三項演算子の形式に修正／例外が発生するメソッドにつきemptyからblankへ修正／式展開の記述へ修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@ module ApplicationHelper
   # ページ毎のtitle表示
   BASE_TITLE = "BIGBAG Store".freeze
   def page_title(page_title)
-    if page_title.empty?
-      BASE_TITLE
-    else
-      page_title + " - " + BASE_TITLE
-    end
+    page_title.blank? ? BASE_TITLE : "#{page_title} - #{BASE_TITLE}"
   end
 end
