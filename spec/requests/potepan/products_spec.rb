629b233ff055b33b896877157d3ffb9b047acf9b
natsuki <su811sh1@gmail.com>
2023-01-23 09:18:26 +0900

    【質問用】リクエストスペックに画像テストを追加

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 RSpec.describe "Potepna::Products", type: :request do
   describe "GET /show" do
     let(:product) { create(:product) }
+    let(:image) { create(:image) }
 
     before do
       get potepan_product_path(product.id)
+      product.images << image
+
+      # 画像URLの取得が上手くいかない問題への対応
+      # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
+      ActiveStorage::Current.host = request.base_url
+      product.reload
+      image.reload
     end
 
     it "商品の詳細画面を表示すること" do
@@ -15,6 +23,11 @@ RSpec.describe "Potepna::Products", type: :request do
     it "正しい商品情報が取得できていること" do
       expect(response.body).to include product.name
       expect(response.body).to include product.price.to_s
+      
+      # binding.pry
+
+      product.images.each { |image| expect(response.body).to include(image.attachment(:large)) }
+      product.images.each { |image| expect(response.body).to include(image.attachment(:small)) }
     end
   end
 end
