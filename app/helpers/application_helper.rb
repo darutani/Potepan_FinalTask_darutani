87c9b07ea29464b2e50638e82c869199a5c2ec91
natsuki <su811sh1@gmail.com>
2023-01-24 15:14:43 +0900

    clean: 定数の下に一行追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   # ページ毎のtitle表示
   BASE_TITLE = "BIGBAG Store".freeze
+
   def page_title(page_title)
     page_title.blank? ? BASE_TITLE : "#{page_title} - #{BASE_TITLE}"
   end
