4538d2bdeec37e82e3e269dea455eecb60414f69
natsuki <su811sh1@gmail.com>
2023-02-02 09:22:58 +0900

    clean: bulletのエラー回避のため、includesからeager_loadに変更

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxonomy.includes(:taxons)
+    @taxonomies = Spree::Taxonomy.eager_load(:taxons)
     @products = @taxon.all_products.includes(master: [:default_price, images: {attachment_attachment: :blob}])
   end
 end
