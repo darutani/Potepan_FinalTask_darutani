0ac18f7bf7f055e56ee4c02c5bed34630645d7be
natsuki <su811sh1@gmail.com>
2023-02-02 09:35:00 +0900

    clean: Rubocopエラー解消のため、ハッシュの左右にスペースを追加

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.eager_load(:taxons)
-    @products = @taxon.all_products.includes(master: [:default_price, images: {attachment_attachment: :blob}])
+    @products = @taxon.all_products.includes(master: [:default_price, images: { attachment_attachment: :blob }])
   end
 end
