d476b5b470f846a990a4f89fc30d33792f414c9f
natsuki <su811sh1@gmail.com>
2023-02-04 11:52:07 +0900

    update: product_decorator.rbに定義したrelated_productsメソッドを呼び出すため変数@related_productsを新規設定

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.includes(master: [images: { attachment_attachment: :blob }]).find(params[:id])
+    @related_products = @product.related_products
   end
 end
