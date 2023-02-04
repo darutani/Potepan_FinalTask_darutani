c4bec320875f9ad82b02c6d79754b7c015dde403
natsuki <su811sh1@gmail.com>
2023-02-04 18:18:11 +0900

    clean: rubocopエラーより、@related_productsの行の長さを短くするため改行を追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,8 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.includes(master: [images: { attachment_attachment: :blob }]).find(params[:id])
-    @related_products = @product.related_products.includes(master: [:default_price, images: { attachment_attachment: :blob }]).limit(4)
+    @related_products = @product.related_products.
+      includes(master: [:default_price, images: { attachment_attachment: :blob }]).
+      limit(4)
   end
 end
-  
