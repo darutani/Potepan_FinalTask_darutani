6ecaeafb999d547c6bcc4061e2a7f0d00378ba50
natsuki <su811sh1@gmail.com>
2023-02-06 17:33:24 +0900

    clean: limitの引数を定数での定義へ変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,8 +1,10 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_DISPLAY_NUMBER = 4
+
   def show
     @product = Spree::Product.includes(master: [images: { attachment_attachment: :blob }]).find(params[:id])
     @related_products = @product.related_products.
       includes(master: [:default_price, images: { attachment_attachment: :blob }]).
-      limit(4)
+      limit(MAX_DISPLAY_NUMBER)
   end
 end
