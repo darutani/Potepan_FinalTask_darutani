f3bf4cb7d7e5cd4206164f468076290e15f1ca47
natsuki <su811sh1@gmail.com>
2023-02-06 17:53:38 +0900

    clean: nameを直接的に参照しているわけではないため、テストデータのnameの指定を削除

--- spec/models/potepan/product_decorator_spec.rb
+++ spec/models/potepan/product_decorator_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductDecorator, type: :model do
   describe "#related_products" do
-    let!(:taxon_jacket) { create(:taxon, name: "jackets") }
-    let!(:taxon_hat) { create(:taxon, name: "hats") }
-    let!(:taxon_html) { create(:taxon, name: "HTML") }
-    let!(:product_jacket_1) { create(:product, name: "potepan-jacket-1", taxons: [taxon_jacket, taxon_html]) }
-    let!(:product_jacket_2) { create(:product, name: "potepan-jacket-2", taxons: [taxon_jacket, taxon_html]) }
-    let!(:product_hat_1) { create(:product, name: "potepan-hat-1", taxons: [taxon_hat, taxon_html]) }
+    let!(:taxon_1) { create(:taxon) }
+    let!(:taxon_2) { create(:taxon) }
+    let!(:taxon_3) { create(:taxon) }
+    let!(:product_1) { create(:product, taxons: [taxon_1, taxon_3]) }
+    let!(:product_2) { create(:product, taxons: [taxon_1, taxon_3]) }
+    let!(:product_3) { create(:product, taxons: [taxon_2, taxon_3]) }
 
     it "同じTaxonに属する商品をもれなく取得すること" do
-      expect(product_jacket_1.related_products).to include product_jacket_2, product_hat_1
+      expect(product_1.related_products).to include product_2, product_3
     end
 
     it "メソッドを呼び出した商品オブジェクト(レシーバー)自体を含まないこと" do
-      expect(product_jacket_1.related_products).not_to include product_jacket_1
+      expect(product_1.related_products).not_to include product_1
     end
 
     it "商品オブジェクトが重複しないこと" do
-      expect(product_jacket_1.related_products).to eq product_jacket_1.related_products.distinct
+      expect(product_1.related_products).to eq product_1.related_products.distinct
     end
   end
 end
