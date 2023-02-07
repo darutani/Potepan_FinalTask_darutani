471ee90924979dd9af802ecc93b18fa08d43dd92
natsuki <su811sh1@gmail.com>
2023-02-07 14:32:34 +0900

    clean: 定数の名前をより分かりやすくするため修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,10 +1,10 @@
 class Potepan::ProductsController < ApplicationController
-  RELATED_PRODUCTS_MAX_DISPLAY_NUMBER = 4
+  MAX_RELATED_PRODUCTS_COUNT = 4
 
   def show
     @product = Spree::Product.includes(master: [images: { attachment_attachment: :blob }]).find(params[:id])
     @related_products = @product.related_products.
       includes(master: [:default_price, images: { attachment_attachment: :blob }]).
-      limit(RELATED_PRODUCTS_MAX_DISPLAY_NUMBER)
+      limit(MAX_RELATED_PRODUCTS_COUNT)
   end
 end
